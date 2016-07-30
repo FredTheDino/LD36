b11a025f7267f5fc116fc9a6d04fb823bca32a98
Edvard T <Edvard.Thornros@gmail.com>
2016-07-30 12:00:45 +0200

    Created dependencies.h

---
+++ src/jampieengine/dependencies.h
@@ -0,0 +1,15 @@
+#include <SDL2/SDL.h>
+
+#ifdef _Win32
+
+#include <GLEW/glew.h>
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
+
+#else
+
+#include <GL/glew.h>
+#include <AL/al.h>
+#include <AL/alc.h>
+
+#endif
